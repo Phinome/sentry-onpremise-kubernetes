6be2115b9cee7c47aff5d558b9794d0ca1cfdf84
Rodrigo P. Fraga <rodrigo@gamifier.co>
2020-11-15 11:52:26 -0300

    -added sentry web, cron, worker.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #------------------------------
-# MICROK8S (Ubuntu Server)
+# VIA MICROK8S (Mac)
+#------------------------------
+microk8s install --mem 4 --cpu 2 --disk 15
+microk8s disable ha-cluster
+microk8s enable dns storage
+
+#Writes the Kubernetes cluster IP on /etc/hosts
+sudo echo "$(multipass info microk8s-vm | grep IPv4 | awk '{ print $2 }')"' k8s.cluster' | sudo tee -a /etc/hosts
+
+#------------------------------
+# VIA MICROK8S (Ubuntu Server)
 #------------------------------
 sudo snap install microk8s --classic
 
@@ -9,6 +19,15 @@ sudo snap install microk8s --classic
 microk8s disable ha-cluster
 microk8s enable dns storage ingress metrics-server
 alias kubectl="microk8s kubectl"
+
+#------------------------------
+# VIA MINIKUBE
+#------------------------------
+minikube start --memory 4096 --cpus 2 --vm-driver=hyperkit
+minikube addons enable ingress
+
+#Writes the Kubernetes minikube cluster IP on /etc/hosts
+sudo echo "$(minikube ip)"' k8s.cluster' | sudo tee -a /etc/hosts
 
 #------------------------------
 # COMMON
